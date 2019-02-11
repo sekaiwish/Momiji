c3d868982e736a8bbd3a485225e9404c662f3bdd
wishu <wish@sek.ai>
2019-02-11 16:53:56 -0600

    upgrade

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import asyncio
 import os
 import random
 import markovify
+import requests
 from pathlib import Path
 from urltest import downloadTxt
 
@@ -32,64 +33,50 @@ async def ping(rx):
 @bot.command(pass_context=True)
 async def dump(rx):
     try:
-        #await bot.say(str(rx.message.channel.name))
-        if rx.message.author.id == "119094696487288833":
+        if rx.message.author.permissions_in(rx.message.channel).administrator:
+            if not os.path.exists(rx.message.channel.id):
+                os.makedirs(rx.message.channel.id)
             i = 0
-            try:
-                c = bot.logs_from(rx.message.channel, limit=int(rx.message.content.split(' ')[1]))
-                print(int(rx.message.content.split(' ')[1]))
-            except:
-                c = bot.logs_from(rx.message.channel, limit=10)
-            filename = "logs/{}img.txt".format(rx.message.channel.id)
-            file = open(filename, 'w')
+            c = bot.logs_from(rx.message.channel, limit=100)
+            await bot.say("Downloading images...")
             async for message in c:
-                try:
-                    url = message.attachments[0]['url']
-                    file.write(url + "\n")
-                    i += 1
-                except:
+                if message.author.bot:
                     pass
-            file.close()
-            await bot.say("downloading...")
-            downloadTxt(filename, rx.message.channel.id)
-            await bot.say("done downloading {} channel images.".format(str(i)))
-        else:
-            await bot.say("sorry, not for u. :cry:")
-    except:
-        pass
-
-@bot.command(pass_context=True)
-async def dump_text(rx):
-    try:
-        #await bot.say(str(rx.message.channel.name))
-        if rx.message.author.id == "119094696487288833":
-            i = 0
-            try:
-                c = bot.logs_from(rx.message.channel, limit=int(rx.message.content.split(' ')[1]))
-                print(int(rx.message.content.split(' ')[1]))
-            except:
-                c = bot.logs_from(rx.message.channel, limit=10)
-            filename = "logs/{}.txt".format(rx.message.channel.id)
-            await bot.say("logging... :gun: pew pew pew haha")
-            file = open(filename, "w")
-            async for message in c:
-                try:
-                    url = message.content
-                    if url == "":
-                        continue
-                    else:
-                        file.write(url + "\n")
+                elif message.attachments:
+                    try:
+                        url = message.attachments[0]["url"]
+                        ext = message.attachments[0]["filename"].split(".")[-1]
+                        r = requests.get(message.attachments[0]["url"], stream=True)
+                        with open("{}/{}.{}".format(rx.message.channel.id, message.id, ext), "wb") as fd:
+                            for chunk in r.iter_content(chunk_size=128):
+                                fd.write(chunk)
                         i += 1
-                        if i % 1000 == 0:
-                            print("downloaded {} lines".format(str(i)))
-                            pass
-                except:
-                    print("error dumping text")
+                        if i % 100 == 0:
+                            print("Downloaded {} images.".format(i))
+                    except:
+                        pass
+                else:
                     pass
+            await bot.say("Collected {} images.".format(i))
+            i = 0
+            c = bot.logs_from(rx.message.channel, limit=1000)
+            await bot.say("Downloading text...")
+            file = open("logs/{}.txt".format(rx.message.channel.id), "w")
+            async for message in c:
+                m = message.clean_content
+                if m == "":
+                    pass
+                elif message.author.bot:
+                    pass
+                else:
+                    file.write(m + "\n")
+                    i += 1
+                    if i % 1000 == 0:
+                        print("Downloaded {} lines.".format(str(i)))
             file.close()
-            await bot.say("collected " + str(i) + " lines of text")
+            await bot.say("Collected {} lines of text.".format(i))
         else:
-            await bot.say("sorry, not for u. :cry:")
+            await bot.say("Administrator only.")
     except:
         pass
 
