0839625818e0de076ade1f93072b5dc873c7a5dc
wish <wish@sek.ai>
2020-06-22 11:00:34 +1000

    lots

--- bot.py
+++ bot.py
@@ -4,33 +4,55 @@ import asyncio
 import os
 import random
 import markovify
-import requests
+import typing
 from subprocess import Popen
 from pathlib import Path
 
 # Startup/Setup
+if os.path.exists("token"):
+    print("Using existing token")
+    with open("token", "r") as fp:
+        token = fp.read()
+else:
+    token = input("Token: ")
+    with open("token", "w") as fp:
+        fp.write(token)
 if not os.path.exists("queue"):
     Path("queue").touch()
-log_path = Path("./logs")
-if not log_path.exists():
-    log_path.mkdir()
-logs = {}
-log_files = os.listdir("logs")
-for log in range(0, len(log_files)):
-    fp = open(f"logs/{log_files[log]}", "r")
-    log_file = log_files[log][:-4]
-    logs[log_file] = fp.readlines()
-    fp.close()
+guild_path = Path("./guild")
+if not guild_path.exists():
+    guild_path.mkdir()
+user_path = Path("./user")
+if not user_path.exists():
+    user_path.mkdir()
+verify_path = Path("./verify")
+if not verify_path.exists():
+    verify_path.mkdir()
+guilds = {}
+for guild in os.scandir("guild"):
+    if guild.is_file():
+        fp = open(f"guild/{guild.name}", "r")
+        guild_id = int(guild.name[:-4])
+        guilds[guild_id] = fp.readlines()
+        fp.close()
+users = {}
+for guild in os.scandir("user"):
+    for user in os.scandir(guild):
+        fp = open(f"user/{guild.name}/{user.name}", "r")
+        user_id = int(user.name[:-4])
+        users[int(guild.name)] = {}
+        users[int(guild.name)][user_id] = fp.readlines()
+        fp.close()
 
 # Initialise dumping queue
-devnull = open(os.devnull, "wb")
-Popen(["nohup", "./dumpqueue.py"], stdout=devnull, stderr=devnull)
+#devnull = open(os.devnull, "wb")
+Popen(["python", "dumpqueue.py"])
 
-bot = commands.Bot(command_prefix=".", owner_id=119094696487288833)
+bot = commands.Bot(command_prefix=";", owner_id=119094696487288833)
 
 @bot.event
 async def on_ready():
-    print(f"I am running on {bot.user.name}\nwith the ID {bot.user.id}")
+    print(f"I am running on {bot.user.name} with the ID {bot.user.id}")
     await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="you."))
 
 @bot.command()
@@ -38,41 +60,38 @@ async def dump(rx):
     try:
         if rx.author.permissions_in(rx.message.channel).administrator or rx.author.id == 119094696487288833:
             i = 0
-            await rx.send("Queueing images...")
-            entries = ""
+            await rx.send("Scanning channel...")
             c = await rx.message.channel.history(limit=None).flatten()
-            for message in c:
-                if message.author.bot:
-                    continue
-                elif message.attachments:
-                    for a in message.attachments:
-                        entries += f"{a.url} {message.channel.id} {message.id}\n"
-                        i += 1
-                else:
-                    pass
-            with open("queue", "a") as fp:
-                fp.write(entries)
-                fp.close()
-            await rx.send("Queued {} images.".format(i))
+            async with rx.message.channel.typing():
+                entries = ""
+                for message in c:
+                    if message.author.bot:
+                        continue
+                    elif message.attachments:
+                        for a in message.attachments:
+                            entries += f"{a.url} {message.channel.id} {message.id}\n"
+                            i += 1
+                with open("queue", "a") as fp:
+                    fp.write(entries)
+                    fp.close()
+                await rx.send(f"Queued {i} images.")
             i = 0
-            await rx.send("Downloading text...")
-            file = open(f"logs/{rx.message.channel.id}.txt", "w")
-            for message in c:
-                m = message.content
-                if m == "":
-                    pass
-                elif message.author.bot:
-                    pass
-                else:
-                    file.write(m + "\n")
-                    i += 1
-                    if i % 1000 == 0:
-                        print("Downloaded {} lines.".format(i))
-            file.close()
-            await rx.send("Collected {} lines of text.".format(i))
+            async with rx.message.channel.typing():
+                file = open(f"guild/{rx.message.channel.id}.txt", "w")
+                for message in c:
+                    m = message.content
+                    if m == "":
+                        continue
+                    elif message.author.bot:
+                        continue
+                    else:
+                        file.write(m + "\n")
+                        i += 1
+                file.close()
+                await rx.send(f"Collected {i} lines of text.")
             log = str(rx.message.channel.id) + ".txt"
-            fp = open(f"logs/{log}", "r")
-            logs[str(rx.message.channel.id)] = fp.readlines()
+            fp = open(f"guild/{log}", "r")
+            guilds[rx.message.channel.id] = fp.readlines()
             fp.close()
         else:
             await rx.send("Administrator only.")
@@ -80,94 +99,136 @@ async def dump(rx):
         pass
 
 @bot.command()
-async def rl(rx):
+async def collect(rx, *, user: discord.Member=-1):
+    if user == -1:
+        user = rx.author
+    elif rx.author.permissions_in(rx.message.channel).administrator or rx.author.id == 119094696487288833:
+        pass
+    else:
+        await rx.send("You are not allowed to collect from other members.")
     try:
-        limit = int(rx.message.content.split(' ')[1])
-        if limit > 15:
-            limit = 15
+        print(f"Now collecting messages for {str(user)} ({user.id})")
+        guild_path = Path(f"user/{rx.message.guild.id}")
+        if not guild_path.exists():
+            guild_path.mkdir()
+        file = open(f"user/{rx.message.guild.id}/{user.id}.txt", "w")
+        await rx.send("Collecting messages...")
+        for channel in rx.guild.text_channels:
+            if channel.permissions_for(rx.message.guild.me).read_message_history:
+                async for message in channel.history(limit=None):
+                    if message.author.id == user.id:
+                        m = message.content
+                        if m == "":
+                            continue
+                        else:
+                            file.write(f"{m}\n")
+        file.close()
+        await rx.send("Messages collected.")
+        fp = open(f"user/{rx.message.guild.id}/{user.id}", "r")
+        users[user.id] = fp.readlines()
+        fp.close()
     except:
+        pass
+
+@bot.command()
+async def rl(rx, *limit):
+    try:
+        if limit:
+            if int(limit[0]) > 15:
+                limit = 15
+            else:
+                limit = int(limit[0])
+        else:
+            limit = 1
+    except ValueError:
         limit = 1
     quotes = ""
-    try:
-        if logs[str(rx.message.channel.id)]:
-            for i in range(limit):
-                quotes += random.choice(logs[str(rx.message.channel.id)])
-            try:
-                await rx.send(quotes)
-            except:
-                await rx.send("Awoo... an error has occurred.")
-        elif os.path.exists(f"logs/{rx.message.channel.id}.txt"):
-            fp = open(f"logs/{rx.message.channel.id}.txt", "r")
-            log = fp.readlines()
-            fp.close()
-            for i in range(limit):
-                quotes += random.choice(log)
-            try:
-                await rx.send(quotes)
-            except:
-                await rx.send("Awoo... an error has occurred.")
-        else:
-            await rx.send("Awoo... this channel has not been dumped.")
-    except KeyError:
-        await rx.send("Awoo... this channel has not been dumped.")
+    for i in range(limit):
+        quotes += get_random_quote(rx)
+    await rx.send(quotes)
 
-def rl_inBot(rx):
+def get_random_quote(rx):
     try:
-        if logs[str(rx.message.channel.id)]:
-            return random.choice(logs[str(rx.message.channel.id)])
-        elif os.path.exists(f"logs/{rx.message.channel.id}.txt"):
-            fp = open(f"logs/{rx.message.channel.id}.txt", "r")
+        if guilds[rx.message.channel.id]:
+            return random.choice(guilds[rx.message.channel.id])
+    except KeyError:
+        if os.path.exists(f"guild/{rx.message.channel.id}.txt"):
+            fp = open(f"guild/{rx.message.channel.id}.txt", "r")
             log = fp.readlines()
             fp.close()
             return random.choice(log)
         else:
             return "Awoo... this channel has not been dumped."
-    except KeyError:
-        return "Awoo... this channel has not been dumped."
 
 @bot.command()
-async def rt(rx):
+async def rm(rx, *, user: discord.Member=-1):
+    if user == -1:
+        user = rx.author
     try:
-        if logs[str(rx.message.channel.id)]:
-            text = "".join(logs[str(rx.message.channel.id)])
-        elif os.path.exists(f"logs/{rx.message.channel.id}.txt"):
-            fp = open(f"logs/{rx.message.channel.id}.txt", "r")
+        if users[rx.message.guild.id][user.id]:
+            text = "".join(users[rx.message.guild.id][user.id])
+    except KeyError:
+        if os.path.exists(f"user/{rx.message.guild.id}/{user.id}.txt"):
+            fp = open(f"user/{rx.message.guild.id}/{user.id}.txt", "r")
+            text = fp.read()
+            fp.close()
+        else:
+            await rx.send("Awoo... this user has not been researched.")
+    if text:
+        await rx.send(markovify.Text(text).make_sentence())
+
+@rm.error
+async def rm_error(rx, error):
+    if isinstance(error, commands.BadArgument):
+        await rx.send("Awoo... user not found.")
+
+@bot.command()
+async def rt(rx, *samples):
+    try:
+        if samples:
+            if int(samples[0]) > 2000:
+                samples = 2000
+            else:
+                samples = int(samples[0])
+        else:
+            samples = 200
+    except ValueError:
+        samples = 200
+    try:
+        if guilds[rx.message.channel.id]:
+            text = "".join(guilds[rx.message.channel.id])
+    except KeyError:
+        if os.path.exists(f"guild/{rx.message.channel.id}.txt"):
+            fp = open(f"guild/{rx.message.channel.id}.txt", "r")
             text = fp.read()
             fp.close()
         else:
             await rx.send("Awoo... this channel has not been dumped.")
-        try:
-            samples = int(rx.message.content.split(' ')[1])
-            if samples > 2000:
-                samples = 2000
-        except:
-            samples = 200
+    if text:
         await rx.send(markovify.Text(text).make_short_sentence(samples))
-    except KeyError:
-        await rx.send("Awoo... this channel has not been dumped.")
 
 @bot.command()
 async def ri(rx):
     directory = str(rx.message.channel.id)
     image = random.choice(os.listdir(directory))
-    file = f"{directory}/{image}"
-    boring = [".ri", ".rl", ".rt", "t!help", "t!slots"]
+    file = f"guild/{directory}/{image}"
+    boring = {".ri", ".rl", ".rt", "t!help", "t!slots"}
     while True:
-        message = rl_inBot(rx)
+        message = get_random_quote(rx)
         if message not in boring:
             break
     try:
-        await rx.send(file=discord.File(file), content=rl_inBot(rx))
+        await rx.send(file=discord.File(file), content=message)
     except:
-        print(f"File '{file}' not found.")
+        print(f"File '{file}' not accessible.")
 
 @bot.command()
 async def rp(rx):
     names = []
     for member in rx.guild.members:
         names.append(member.name)
-    text1 = rl_inBot(rx)
-    text2 = rl_inBot(rx)
+    text1 = get_random_quote(rx)
+    text2 = get_random_quote(rx)
     name1 = random.choice(names)
     name2 = random.choice(names)
     if name1 == name2:
@@ -176,4 +237,13 @@ async def rp(rx):
         final = f"{name1}: {text1}\n{name2}: {text2}"
     await rx.send(final)
 
-bot.run("[REDACTED]")
+@bot.command()
+async def verify(rx):
+    image = random.choice(os.listdir("verify"))
+    file = f"verify/{image}"
+    try:
+        await rx.send(file=discord.File(file))
+    except:
+        print(f"File '{file}' not accessible.")
+
+bot.run(token)
--- dumpqueue.py
+++ dumpqueue.py
@@ -2,7 +2,7 @@ import requests
 import time
 from pathlib import Path
 
-valid = ["png", "jpg", "jpeg", "gif", "webm", "mov", "mp4"]
+valid = {"png", "jpg", "jpeg", "gif", "webm", "mov", "mp4"}
 
 while True:
     with open("queue", "r+") as fp:
@@ -17,13 +17,13 @@ while True:
                 file = line.split(" ")
                 # Format: url channel_id message_id
                 if file[0].split(".")[-1] in valid:
-                    channel_path = Path("./" + file[1])
+                    channel_path = Path("./guild/" + file[1])
                     if not channel_path.exists():
                         channel_path.mkdir()
-                    file_path = Path("./{}/{}.{}".format(file[1], file[2], file[0].split(".")[-1]))
+                    file_path = Path("./guild/{}/{}.{}".format(file[1], file[2], file[0].split(".")[-1]))
                     if not file_path.exists():
                         r = requests.get(file[0], stream=True)
-                        with open("{}/{}.{}".format(file[1], file[2], file[0].split(".")[-1]), "wb") as fd:
+                        with open(file_path, "wb") as fd:
                             for chunk in r.iter_content(chunk_size=128):
                                 fd.write(chunk)
             except:
